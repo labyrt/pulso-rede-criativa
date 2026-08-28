b759f099f98fd0c91bbd7f4ef15f1795f6893507
Lucy Mazzini Lessa <labyrtcontact@gmail.com>
2026-08-27 23:45:53 -0300

    feat: expor websocket global de eventos

--- apps/chat/routing.py
+++ apps/chat/routing.py
@@ -1,7 +1,9 @@
 from django.urls import re_path
 
 from .consumers import ConversationConsumer
+from .events import UserEventsConsumer
 
 websocket_urlpatterns = [
+    re_path(r"^ws/events/$", UserEventsConsumer.as_asgi()),
     re_path(r"^ws/chat/(?P<conversation_id>\d+)/$", ConversationConsumer.as_asgi()),
 ]
